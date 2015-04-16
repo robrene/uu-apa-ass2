09e086fb7337b534ddbf311c9419477a9b2d2457
Rob Spoel <robspoel@gmail.com>
2015-04-16 16:06:49 +0200

    Changed Fun language to be in line with NNH chapter 5.

--- Fun.hs
+++ Fun.hs
@@ -1,35 +1,26 @@
 module Fun (
-    Const (..)
+    Exp (..)
+  , Const (..)
   , Var
-  , Lab
-  , Exp (..)
-  , Term (..)
+  , Pnt
   , Op (..)
   )
 where
 
-import Data.Hashable
-
-data Const = Bool Bool | Int Int
-  deriving (Eq, Show)
-type Var = String
-type Lab = Integer
-
-data Exp = Exp Term Lab
-  deriving (Eq, Show)
-
-data Term =
+data Exp =
     Const Const
   | Var Var
-  | Fn Var Exp       -- "regular" function
-  | Fun Var Var Exp  -- recursive function
+  | Fn Pnt Var Exp
+  | Fun Pnt Var Var Exp
   | App Exp Exp
   | ITE Exp Exp Exp
-  | Let Var Exp Exp
+  | Let Var Exp
   | Op Exp Op Exp
   deriving (Eq, Show)
-  -- TODO: Add Terms for data structures (lists, pairs, general data types)
-  -- TODO: Add Term for case statement
+
+data Const = CInt Int | CBool Bool deriving (Eq, Show)
+type Var = String
+type Pnt = String
 
 data Op =
     Eq
@@ -45,19 +36,3 @@ data Op =
   | And
   | Or
   deriving (Eq, Show)
-
-instance Hashable Const where
-  hash = hash . show
-  hashWithSalt s = hashWithSalt s . show
-
-instance Hashable Exp where
-  hash = hash . show
-  hashWithSalt s = hashWithSalt s . show
-
-instance Hashable Term where
-  hash = hash . show
-  hashWithSalt s = hashWithSalt s . show
-
-instance Hashable Op where
-  hash = hash . show
-  hashWithSalt s = hashWithSalt s . show
